7da51a5cb2779672d0729db12c98e5c3f896693a
ahmedahmedoumer <ahmedinoumer13@gmail.com>
2024-03-07 00:04:34 +0300

    editing and configuring

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       // Insert some stuff
+        DB::table('users')->insert(
+            array(
+                'firstname' => 'William',
+                'lastname' => 'Castillo',
+                'username' => 'William Castillo',
+                'email' => 'hasen@admin.com',
+                'password' => Hash::make(12345678),
+                'avatar' => 'no_avatar.png',
+                'phone' => '0123456789',
+                'role_id' => 1,
+                'statut' => 1,
+            )
+        );
+    }
+}
